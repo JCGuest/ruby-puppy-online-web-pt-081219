cffd0e6f68e2b386e1bccfdd7ec9a6753437d559
John Guest <jcguest90@gmail.com>
2019-08-29 03:52:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
-# Add your code here
+class Dog 
+  
+attr_accessor :name 
+
+@@all = []
+
+@@names = []
+
+def initialize(name)
+  @name = name
+  @@all << self
+  @@names << name
+  
+end
+
+
+def self.all 
+  @@all
+end
+
+def self.clear_all
+  @@all = []
+end
+
+def self.print_all
+  print @@names.uniq.to_s
+end 
+
+
+end 
